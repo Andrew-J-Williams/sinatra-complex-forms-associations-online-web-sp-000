6681a5ffa42f09a3c9bb22d7ae219a3315d9a92f
Andrew Williams <1williamsaj@gmail.com>
2020-01-26 02:10:23 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -33,6 +33,8 @@ class PetsController < ApplicationController
   end
 
   patch '/pets/:id' do
+    
+
     @pet = Pet.find(params[:id])
     @pet.update(params["pet"])
     if !params["owner"]["name"].empty?
