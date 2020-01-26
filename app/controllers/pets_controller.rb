4d769a770cebd09c849027dae628b6e50ff4ce00
Andrew Williams <1williamsaj@gmail.com>
2020-01-26 01:57:34 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -27,7 +27,7 @@ class PetsController < ApplicationController
   end
 
   get '/pets/:id/edit' do
-    @pets = Pet.find(params[:id]
+    @pets = Pet.find(params[:id])
     @owner = Owner.all
     erb :'/pets/edit'
   end
